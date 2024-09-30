f8af5d9eddc9ebe3d2b2b84930fad7c84751f79b
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-09-30 09:14:10 +0300

    commit

---
+++ oop_11/oop_11/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("Число додатнє.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("Число від'ємне.");
+        }
+        else
+        {
+            Console.WriteLine("Число дорівнює нулю.");
+        }
+    }
+}
