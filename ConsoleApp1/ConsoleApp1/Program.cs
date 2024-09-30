eb23c5b96e8d0fdf1c8a3138112b21d3bcdb3f6e
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-09-30 09:21:58 +0300

    commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿//2
+int year;
+
+Console.Write("Введіть рік: ");
+year = int.Parse(Console.ReadLine());
+
+bool isLeapYear = (year % 4 == 0 && year % 100 != 0);
+
+if (isLeapYear)
+{
+    Console.WriteLine(year + " є високосним роком");
+}
+else
+{
+    Console.WriteLine(year + " не є високосним роком");
+}
