f7a513284fc231e346cef1dbe59fa4fc5e11be77
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-09-30 10:23:13 +0300

    commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿int num1, num2, num3;
+
+Console.Write("Введіть перше число: ");
+num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введіть друге число: ");
+num2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введіть третє число: ");
+num3 = int.Parse(Console.ReadLine());
+
+int max = Math.Max(num1, Math.Max(num2, num3));
+
+Console.WriteLine("Максимальне число: " + max);
