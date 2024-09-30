52eece600cf8256aba108c8771d4b01159968d46
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-09-30 10:25:37 +0300

    commit

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введіть вік: ");
+int age = int.Parse(Console.ReadLine());
+
+if (age >= 12 && age <= 18)
+{
+    Console.WriteLine("Ти підліток");
+}
+else
+{
+    Console.WriteLine("Ти не підліток");
+}
