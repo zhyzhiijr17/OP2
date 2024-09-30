1678b18d21872be73a974a3139eef35c95177186
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-09-30 09:40:55 +0300

    commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введіть букву: ");
+char inputChar = Console.ReadKey().KeyChar;
+Console.WriteLine();
+
+bool isVowel = false;
+
+switch (Char.ToLower(inputChar))
+{
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        isVowel = true;
+        break;
+}
+if (isVowel)
+{
+    Console.WriteLine("Це голосна буква");
+}
+else
+{
+    Console.WriteLine("Це приголосна буква");
+}
