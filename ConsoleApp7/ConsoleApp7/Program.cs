d1e836ae06a0fef7991b504cb52349c346dadbc1
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-09-30 10:30:03 +0300

    commit

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введіть довжину сторони a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть довжину сторони b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть довжину сторони c: ");
+double c = double.Parse(Console.ReadLine());
+
+if (a >= b && a >= c)
+{
+    Console.WriteLine("Сторона a є найдовшою");
+}
+else if (b >= a && b >= c)
+{
+    Console.WriteLine("Сторона b є найдовшою");
+}
+else
+{
+    Console.WriteLine("Сторона c є найдовшою");
+}
