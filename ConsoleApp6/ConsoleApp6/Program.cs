8e24df0fa756a6ca4a899fd02a6f3cede9e8f1da
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-09-30 10:27:27 +0300

    commit

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введіть число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число є парним");
+}
+else
+{
+    Console.WriteLine("Число є непарним");
+}
+
+if (number % 3 == 0)
+{
+    Console.WriteLine("Число ділиться на 3");
+}
+else
+{
+    Console.WriteLine("Число не ділиться на 3");
+}
